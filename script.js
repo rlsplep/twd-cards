1258ea48db82bd4511fbe1eda9712c6eb77fdc7a
develcode <163679849+rlsplep@users.noreply.github.com>
2026-04-24 18:24:47 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const characters = ["Клементина", "Ли", "Кенни"]
+const images = [
+    "https://upload.wikimedia.org/wikipedia/ru/thumb/f/fc/%D0%9A%D0%BB%D0%B5%D0%BC%D0%B5%D0%BD%D1%82%D0%B8%D0%BD%D0%B0_The_walking_dead_the_final_season.jpg/330px-%D0%9A%D0%BB%D0%B5%D0%BC%D0%B5%D0%BD%D1%82%D0%B8%D0%BD%D0%B0_The_walking_dead_the_final_season.jpg",
+    "https://upload.wikimedia.org/wikipedia/ru/0/0e/%D0%9B%D0%B8_%D0%AD%D0%B2%D0%B5%D1%80%D0%B5%D1%82%D1%82.jpg",
+    "https://i.pinimg.com/736x/9b/93/4b/9b934b82a46ddaf53da34c0f5e594930.jpg"
+];
+
 let currentIndex = 0;
 
 const nameElement = document.getElementById("char-name");
+const imgElement = document.getElementById('char-img');
 const nextBtn = document.querySelector(".right-arrow");
 const prevBtn = document.querySelector(".left-arrow");
 
@@ -13,6 +20,7 @@ nextBtn.addEventListener("click", function () {
     }
 
     nameElement.textContent = characters[currentIndex];
+    imgElement.src = images[currentIndex]
 
 });
 
@@ -24,6 +32,7 @@ prevBtn.addEventListener("click", function () {
     }
 
     nameElement.textContent = characters[currentIndex];
+    imgElement.src = images[currentIndex]
 
 });   
 
