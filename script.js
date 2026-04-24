d975acbbb60de9dfe3f420f2a0a4415e4f65474e
develcode <163679849+rlsplep@users.noreply.github.com>
2026-04-24 17:51:48 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
-const nameElement = document.getElementById("char-name");
+const characters = ["Клементина", "Ли", "Кенни"]
+let currentIndex = 0;
 
+const nameElement = document.getElementById("char-name");
 const nextBtn = document.querySelector(".right-arrow");
+const prevBtn = document.querySelector(".left-arrow");
 
 nextBtn.addEventListener("click", function () {
-    
-    if (nameElement.textContent === "Клементина") {
-        nameElement.textContent = "Ли";
-    } else {
-        nameElement.textContent = "Клементина";
+    currentIndex = currentIndex + 1;
+
+    if (currentIndex >= characters.length) {
+        currentIndex = 0;
     }
-    console.log("Имя изменено!");
+
+    nameElement.textContent = characters[currentIndex];
+
+});
+
+prevBtn.addEventListener("click", function () {
+    currentIndex = currentIndex - 1;
+
+    if (currentIndex < 0) {
+        currentIndex = characters.length - 1;
+    }
+
+    nameElement.textContent = characters[currentIndex];
+
+});   
+
+
+
+
+
+
+
+
+
     
-});    
